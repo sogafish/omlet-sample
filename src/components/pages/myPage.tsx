8696173d8e02182ecbcf5f265ae43063ad7e8c6f
sogafish <kent.soga@gmail.com>
2024-11-07 01:19:57 +0900

    Change Props

--- src/components/pages/myPage.tsx
+++ src/components/pages/myPage.tsx
@@ -1,12 +1,16 @@
+import { useMemo } from "react";
 import { CustomButton } from "../custom-button";
 
 export function MyPage() {
+  const randomNumber = useMemo(() => {
+    return Math.round(Math.random() * 10);
+  }, []);
   return (
     <div>
       <CustomButton
         label="Click!!!"
         onClick={() => console.info("clicked")}
-        unusedProp={30}
+        unusedProp={randomNumber}
       />
     </div>
   );
